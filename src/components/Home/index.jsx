2666f12165e0db53a3b42b7dc3a2c037c474c048
Aditya Ganti <adi10rox@gmail.com>
2022-02-24 17:51:30 +0530

    cards cleanup

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -6,21 +6,17 @@ import "./index.scss";
 
 export default function Home() {
     const poseRef = useFirestore().collection("poses");
-    console.log(poseRef);
     const poses = useFirestoreCollectionData(poseRef); 
-    console.log(poses); 
 
     return (
         <Suspense>
             <div className="home">
                 <div className="home_heading">Standalone Poses</div>
                 <div className="card_container">
-                    {poses.data?.map((pose) => {
-                        <Card title={pose.name} onClick={()=>console.log("click")} path={pose.image_url?pose.image_url:""} />
-                        
-                    })}
-                    <Card title="Sample" onClick="" path="poses/Staff.jpg" />
-                    <Card title="Sample" onClick="" path="poses/Staff.jpg" />
+                    {poses.data?.map((pose) => (
+                        <Card title={pose.name} onClick={() => console.log("click")} path={pose.image_url} />
+                    ))}
+                    <Card title="Warrior 2 Pose" onClick="" path="poses/Warrior2.jpg"/>
                 </div>
             </div>
         </Suspense>
